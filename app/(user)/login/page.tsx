d017dffa47123c0308dd5972314fba9f685d1b22
hi-aa <ost.jj1201@gmail.com>
2024-09-15 11:46:50 +0900

    add style2 - write

--- app/(user)/login/page.tsx
+++ app/(user)/login/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Login() {
   return (
     <>
@@ -48,12 +50,18 @@ export default function Login() {
             Remember me
           </label>
         </div>
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Submit
-        </button>
+
+        <div className="flex justify-between ">
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+          <Link href={"/join"} className="text-sm">
+            회원가입
+          </Link>
+        </div>
       </form>
     </>
   );
